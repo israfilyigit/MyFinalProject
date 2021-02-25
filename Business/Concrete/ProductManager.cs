4a8a82b69395df9b365d587ae313ba4cf1e30964
snipiso snipiso <israfilyigit@gmail.com>
2021-02-25 14:35:37 +0300

    10

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -16,24 +18,45 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        public IResult Add(Product product)
+        {
+            if (product.ProductName.Length<2)
+            {
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
 
-        public List<Product> GetAll()
-        {
-            return _productDal.GetAll();
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetAll(p=>p.CategoryId==id);
+   
+
+            return new SuccsessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails(),Messages.ProductsListed);
         }
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice<=max);
+            return new SuccsessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==id));
+        }
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccsessDataResult<Product>(_productDal.Get(p => p.ProductId == productId),Messages.ProductAdded);
+        }
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
+        {
+            return new SuccsessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice<=max));
+        }
+        public IDataResult<List<Product>> GetAll()
+        {
+            if (DateTime.Now.Hour == 10)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+            return new SuccsessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
         }
 
        
-        public List<ProductDetailDto> GetProductDetails()
-        {
-            return _productDal.GetProductDetails();
-        }
+
+
     }
 }
